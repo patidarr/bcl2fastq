542df63745d4153e2c7fbbb152b5bf74c7281188
Rajesh Patidar <rajbtpatidar@gmail.com>
2017-09-07 15:59:02 -0400

    Minor changes

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -6,6 +6,6 @@ module load snakemake
 export TARGET="$target"
 snakemake -r -p --snakefile /users/n2000747426/patidarr/bcl2fastq/bcl2fastq.snakemake\
 	--nolock  --ri -k -p -T -r -j 3000\
-	--cluster "qsub -W umask=022 -V -o /users/n2000747426/patidarr/bcl2fastq/log/ -e /users/n2000747426/patidarr/bcl2fastq/log/ {params.batch}"\
+	--cluster "qsub -W umask=022 -V -o /users/n2000747426/patidarr/bcl2fastq/log/{params.rulename}.%j.o -e /users/n2000747426/patidarr/bcl2fastq/log/{params.rulename}.%j.o {params.batch}"\
 	--stats /users/n2000747426/patidarr/bcl2fastq/log/${time}.stats\
 	$target >& /users/n2000747426/patidarr/bcl2fastq/log/${time}.log
