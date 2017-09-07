1834e0cd585e787d51d555344bae8920faf744d8
Patidar <patidarr@fr-s-hpc-a1-42.ncifcrf.gov>
2017-09-07 15:04:35 -0400

    Running as expected, remove patidar in future from Unaligned Name

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #PBS -S /bin/bash
 source /etc/profile.d/modules.sh
-time=`date +"%Y%m%d"`
+time=`date +"%Y%m%d_%H%M%S"`
 module load snakemake
 export TARGET="$target"
 snakemake -r -p --snakefile /users/n2000747426/patidarr/bcl2fastq/bcl2fastq.snakemake\
 	--nolock  --ri -k -p -T -r -j 3000\
-	--cluster "qsub -W umask=022 -V -o log/ -e log/ {params.batch}"\
-	--stats ${time}.stats\
-	$target >& ${time}.log
+	--cluster "qsub -W umask=022 -V -o /users/n2000747426/patidarr/bcl2fastq/log/ -e /users/n2000747426/patidarr/bcl2fastq/log/ {params.batch}"\
+	--stats /users/n2000747426/patidarr/bcl2fastq/log/${time}.stats\
+	$target >& /users/n2000747426/patidarr/bcl2fastq/log/${time}.log
